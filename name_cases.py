8bcd68687e13e04b6c12b437231eeb04144bd954
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-06 14:06:47 -0500

    Add files via upload
    Starting with basic variables (Chapters 1-3 of Python Crash Course)

---
+++ name_cases.py
@@ -0,0 +1,6 @@
+# Use a variable to represent a person's name, then print in uppercase, lowercase, titlecase
+
+mom = "AnN aNdReWs"
+print(mom.upper())
+print(mom.lower())
+print(mom.title())
---
+++ simple_message.py
@@ -0,0 +1,8 @@
+test_variable = "Testing again for posterity"
+print(test_variable)
+
+quotations = 'I told my friend, "Python is my favorite language!"'
+apostrophes = "The language 'Python' is named after Monty Python."
+possession = "One of Python's strengths is its diverse and supportive community."
+
+print(possession)   
