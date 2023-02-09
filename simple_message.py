6f970b58055c6a3b390834555dbe95689a00dc81
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-08 20:08:34 -0500

    Delete simple_message.py

--- simple_message.py
+++
@@ -1,8 +0,0 @@
-test_variable = "Testing again for posterity"
-print(test_variable)
-
-quotations = 'I told my friend, "Python is my favorite language!"'
-apostrophes = "The language 'Python' is named after Monty Python."
-possession = "One of Python's strengths is its diverse and supportive community."
-
-print(possession)   
