f912abf6684c18b5ad6aa52f78f496261bd450ff
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-08 20:08:09 -0500

    Delete names.py

--- names.py
+++
@@ -1,7 +0,0 @@
-# Store the names of a few friends in a list called names. Print each name one at a time.
-
-names = ['morgan Shankle', 'Gwen smith', 'abbi Ogden']
-
-print(names[0].title())
-print(names[1].title())
-print(names[2].title())
