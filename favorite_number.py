362b5a54af35f436ee8f386ac3b425b56caaa97a
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-08 20:06:53 -0500

    Delete favorite_number.py

--- favorite_number.py
+++
@@ -1,6 +0,0 @@
-# Use a variable to represent a number, then print a message revealing your number.
-
-MY_NUMBER = 21
-
-number_is = f"Multiplying 7 and 3 equals {MY_NUMBER}, which is my number."
-print(number_is)
