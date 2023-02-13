1a6cfa6def6a297ddff8fd700dccbb4560499a8d
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-13 17:30:56 -0500

    Add files via upload

---
+++ dictionaries/favorite_numbers.py
@@ -0,0 +1,16 @@
+# Use a dictionary to store 5 people's favorite numbers
+# Print each person's name and their favorite number
+
+favorite_numbers = {
+    'corey' : 32,
+    'ellen' : 29,
+    'joseph' : 28,
+    'maya' : 34,
+    'alex' : 37,
+    }
+
+print(f"Corey's favorite number is {favorite_numbers['corey']}.")
+print(f"Maya's favorite number is {favorite_numbers['maya']}.")
+print(f"Alex's favorite number is {favorite_numbers['alex']}.")
+print(f"Ellen's favorite number is {favorite_numbers['ellen']}.")
+print(f"Joseph's favorite number is {favorite_numbers['joseph']}.")
---
+++ dictionaries/person.py
@@ -0,0 +1,15 @@
+# Use a dictionary to store information about a person you know
+# # Store first name, last name, age, city
+# Print each piece of information stored in dictionary
+
+gwen_dictionary = {
+    'first_name' : 'gwen', 
+    'last_name' : 'smith', 
+    'age' : 34,
+    'city' : 'mauldin'
+    }
+
+print(gwen_dictionary['first_name'].title())
+print(gwen_dictionary['last_name'].title())
+print(gwen_dictionary['age'])
+print(gwen_dictionary['city'].title())
