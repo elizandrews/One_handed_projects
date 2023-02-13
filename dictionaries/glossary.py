95b4f60ad7fdd729bff916f0c08d7bc0335f5b30
elizandrews <61242894+elizandrews@users.noreply.github.com>
2023-02-13 17:29:43 -0500

    Create glossary.py

---
+++ dictionaries/glossary.py
@@ -0,0 +1,16 @@
+# Think of 5 programming terms to use as keys and their definitions as values
+# Print each word and its meaning in neatly formatted output
+
+glossary = {
+    'list' : 'a data-type used to store multiple items in a single variable',
+    'variable' : 'a symbolic name used as a pointer to an object',
+    'tuple' : 'an immutable list',
+    'dictionary' : 'a data-type that stores key-value pairs',
+    'python' : 'a high level, general purpose programming language',
+    }
+
+print(f"A list is {glossary['list']}.")
+print(f"\nA variable is {glossary['variable']}.")
+print(f"\nA tuple is {glossary['tuple']}.")
+print(f"\nA dictionary is {glossary['dictionary']}.")
+print(f"\nPython is {glossary['python']}.")
